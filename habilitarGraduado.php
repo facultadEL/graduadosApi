3915520a9af8d2610b49dc4274da2dd158b69ca2
Eze BIT <efigueroa@bit.com.ar>
2016-05-19 11:53:15 -0300

    Count quantity

--- habilitarGraduado.php
+++ habilitarGraduado.php
@@ -26,14 +26,21 @@ pg_query($termino);
 if($error == 1)
 {
 	$success = 'f';
+    $count = 0;
 }
 else
 {
+    $c = "SELECT count(id_alumno) as cant FROM alumno WHERE habilitado IS FALSE AND regional_fk=(SELECT regional_fk FROM alumno WHERE id_alumno='$id');";
+    $s = pg_query($c);
+    $r = pg_fetch_array($s);
+    $count = $r['cant'];
+    
     $success = 't';
 }
 
 $outJson = '[{
-	"success":"'.$success.'"
+	"success":"'.$success.'",
+    "cant":"'.$count.'"
 }]';
 
 pg_close($conn);
