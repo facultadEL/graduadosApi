15058757a3b36c69217a93b1b27e05e2ce06484b
luponce17 <luponce17@gmail.com>
2016-06-14 13:04:17 -0300

    Get rubros added

---
+++ getRubro.php
@@ -0,0 +1,31 @@
+<?php
+header('Access-Control-Allow-Origin: *'); //Esto va cada vez para asegurarse que permita las conexiones de afuera
+include_once "conexion.php";
+
+$rubro = $REQUEST['rubro'];
+
+$c = "SELECT * FROM rubro";
+$s = pg_query($c);
+
+$outJson = "[";
+while ($r = pg_fetch_array($s)) 
+{
+    if ($outJson != "[")
+    {
+        $outJson .= ",";
+    }
+
+    $id = $r['id'];
+    $nombre = $r['nombre'];
+
+    $outJson .= '{
+        "id":"'.$id.'",
+        "nombre":"'.$nombre.'"
+    }';
+}
+
+$outJson .= "]";
+pg_close($conn);
+echo $outJson;
+
+?>
