6322e6e16f8e70ae8303cea5719660d6b74bb68f
Eze BIT <efigueroa@bit.com.ar>
2016-07-06 10:35:24 -0300

    Set registration id

---
+++ setRegId.php
@@ -0,0 +1,29 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+include_once 'conexion.php';
+
+$id = $_REQUEST['id'];
+$regId = $_REQUEST['regId'];
+
+$c = "UPDATE alumno SET registration_id='$regId' WHERE id_alumno='$id';";
+
+$success = 't';
+
+if (!pg_query($c)){
+	$errorpg = pg_last_error($conn);
+	$termino = "ROLLBACK";
+	$success = 'f';
+}else{
+	$termino = "COMMIT";
+}
+pg_query($termino);
+
+$outJson = '[{
+    "success":"'.$success.'"
+}]';
+
+pg_close($conn);
+
+echo $outJson;
+
+?>
